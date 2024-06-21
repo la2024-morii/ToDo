2ea18b3ec12089024402e5cc64a299f6d9a6883c
AP-0117\student <461-student008@la-study.com>
2024-06-21 15:42:22 +0900

    コントローラの変更

--- ToDo/src/main/java/com/example/demo/controller/CategoryController.java
+++ ToDo/src/main/java/com/example/demo/controller/CategoryController.java
@@ -1,13 +1,27 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.entity.Category;
+import com.example.demo.repository.CategoryRepository;
 
 @Controller
 public class CategoryController {
+	
+	@Autowired
+	CategoryRepository categoryRepository;
+	
 	@GetMapping("/category")
-	public String index() {
+	public String index(Model model) {
+		List<Category> CategoryList = categoryRepository.findAll();
+		model.addAttribute("CategoryList", CategoryList);
 		return "category/category";
 	}
 
@@ -17,7 +31,13 @@ public class CategoryController {
 	}
 
 	@PostMapping("/category/add")
-	public String store() {
+	public String store(
+			@RequestParam("newCategoryWord") String newCategoryWord,
+			Model model
+			) {
+		Category category =new Category(newCategoryWord);
+		categoryRepository.save(category);
+		
 		return "redirect:/todo/{categolyId}";
 	}
 
