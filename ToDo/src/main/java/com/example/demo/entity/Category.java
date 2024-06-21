b97a0e176bc3455940adaf4897f94af8eaac08b4
AP-0117\student <461-student016@la-study.com>
2024-06-21 13:53:03 +0900

    CategoryのEntityとRepositoryを作成

---
+++ ToDo/src/main/java/com/example/demo/entity/Category.java
@@ -0,0 +1,47 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "category")
+public class Category {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "category_id")
+	private Integer categoryId;
+	
+	@Column(name = "category_name")
+	private String categoryName;
+
+	public Category() {
+		
+	}
+
+	public Category(Integer categoryId, String categoryName) {
+		this.categoryId = categoryId;
+		this.categoryName = categoryName;
+	}
+
+	public Integer getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(Integer categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+}
