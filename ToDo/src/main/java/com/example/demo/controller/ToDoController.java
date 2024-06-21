998712649e955d61a50f92da2d1d4c1c939733f4
AP-0117\student <461-student037@la-study.com>
2024-06-21 14:35:24 +0900

    コントローラとフォルダ

--- ToDo/src/main/java/com/example/demo/controller/ToDoController.java
+++ ToDo/src/main/java/com/example/demo/controller/ToDoController.java
@@ -14,19 +14,58 @@ public class ToDoController {
 	//toDo.html
 	@GetMapping("/{categolyId}")
 	public String index(
-			@PathVariable("categoryId")Integer categoryId) {
-		return "toDo";
+			@PathVariable("categoryId") Integer categoryId) {
+		return "todo/toDo";
 	}
-	
+
 	//[POST]"/{categoryId}/{toDoId}/check"
 	//check()
 	//redirect/:todo/{caregoryId}
 	@PostMapping("/{categoryId}/{toDoId}/check")
 	public String check(
-			@PathVariable("categoryId")Integer categoryId,
-			@PathVariable("toDoId")Integer toDoId) {
-		return "redirect/:{categoryId}";
+			@PathVariable("categoryId") Integer categoryId,
+			@PathVariable("toDoId") Integer toDoId) {
+		return "redirect:/{categoryId}";
 	}
-	
+
+	@GetMapping("/{categoryId}/add")
+	public String create() {
+		return "todo/addToDo";
+	}
+
+	@PostMapping("/{categoryId}/add")
+	public String store() {
+		return "redirect:/{categoryId}";
+	}
+
+	//[GET]"/todo/{categoryId}/{toDoId}/edit"
+	//	edit()
+	//editToDo.html
+	@GetMapping("/{categoryId}/{toDoID}/edit")
+	public String edit() {
+		return "todo/editToDo";
+	}
+
+	//[POST]"/todo/{categoryId}/{toDoId}/edit"
+	//	update()
+	@PostMapping("/{categoryId}/{toDoId}/edit")
+	public String update() {
+		return "redirect:/{categoryId}";
+
+	}
+
+	//[POST]"/todo/{categoryId}/{toDoId}/delete"
+	//	delete()
+	@PostMapping("/{categoryId}/{toDoId}/delete")
+	public String delete() {
+		return "redirect:/{categoryId}";
+	}
+	//[GET]"/todo/{categoryId}/complete"
+	//	complrte()
+	//completeToDo.html
+	@GetMapping("/{categoryId}/complete")
+	public String complete() {
+		return "todo/completeToDo";
+	}
 
 }
