9143890f49ba8aa177f453d28011f13c3ce00bc0
AP-0117\student <461-student037@la-study.com>
2024-06-24 09:27:19 +0900

    Revert "update"
    This reverts commit 2063bf7e273d7de187838e1d636fcba8171076c5.

--- ToDo/src/main/java/com/example/demo/controller/ToDoController.java
+++ ToDo/src/main/java/com/example/demo/controller/ToDoController.java
@@ -59,8 +59,6 @@ public class ToDoController {
 			@PathVariable("categoryId")Integer categoryId,
 			@RequestParam("categoryName")String categoryName,
 			@RequestParam("name")String name) {
-		ToDo todo = new ToDo(name, categoryId, 1);
-		this.toDoRepository.save(todo);
 		return "redirect:/{categoryId}";
 	}
 
